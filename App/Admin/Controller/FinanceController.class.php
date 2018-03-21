7f6a10df7fc56638087a4f0ff8dd27854aa144c4
elickzhao <xwiwi@foxmail.com>
2018-03-21 22:33:20 +0800

    财务管理里的对账单下载功能全部完毕 更新了 phpcxcel 因为和php7.0有冲突

--- App/Admin/Controller/FinanceController.class.php
+++ App/Admin/Controller/FinanceController.class.php
@@ -37,7 +37,6 @@ class FinanceController extends PublicController
 
     public function getData()
     {
-
         $status = ['SUCCESS'=>'支付成功','REFUND'=>'转入退款','NOTPAY'=>'未支付','CLOSED'=>'已关闭','PAYERROR'=>'支付失败(其他原因，如银行返回失败)'];
 
         $did = $_REQUEST["transaction_id"];
@@ -66,7 +65,6 @@ class FinanceController extends PublicController
 
     public function getRefund()
     {
-
         $status = ['SUCCESS'=>'退款成功','REFUNDCLOSE'=>'退款关闭','PROCESSING'=>'退款处理中','CHANGE'=>'退款异常，退款到银行发现用户的卡作废或者冻结了，导致原路退款银行卡失败'];
         $channel = ['ORIGINAL'=>'原路退款','BALANCE'=>'退回到余额','OTHER_BALANCE'=>'原账户异常退到其他余额账户','OTHER_BANKCARD'=>'原银行卡异常退到其他银行卡'];
 
@@ -109,14 +107,117 @@ class FinanceController extends PublicController
                 $input->SetBill_date($bill_date);
                 $input->SetBill_type($bill_type);
                 $file = \WxPayApi::downloadBill($input);
-                echo $file;
-                //TODO 对账单文件处理
+                $file = explode("\r\n", $file);
+                if ($file[0] != "") {
+                    $this->dealWechat($file, $_REQUEST["bill_type"]);
+                } else {
+                    $this->error('没有相应的对账单可供下载', '', 1.5);
+                }
                 exit(0);
             }
         }
 
+        //dump($this->deal_wechat_return_result($file));
+        //$this->deal_wechat_return_result($file);
+
         $bc = ['财务管理','下载对账单'];
         $this->assign('bc', $bc);
         $this->display();
+    }
+
+    public function dealWechat($arr, $title)
+    {
+        $cellName = explode(",", $arr[0]);
+        $data =  str_replace(",", " ", array_slice($arr, 1, count($arr)-3));    //订单数据
+        $total =  str_replace(",", " ", array_slice($arr, -3, 2));              //统计数据  网上记得改成3
+        $data_nums = count($data);  //行数 用于计算统计起始行
+
+        //引入核心文件
+        vendor("PHPExcel.PHPExcel");
+        $objPHPExcel = new \PHPExcel();
+        //定义配置
+        $topNumber = 2;//表头在第几行
+        $xlsTitle = iconv('utf-8', 'gb2312', $title);//文件名称
+        $fileName = $title.date('_YmdHis');//文件名称
+        $cellKey = array(
+                'A','B','C','D','E','F','G','H','I','J','K','L','M',
+                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                'AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM',
+                'AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ'
+                );
+        //标题头设置高度
+        $objPHPExcel->getActiveSheet()->getRowDimension('1')->setRowHeight(30);//设置行高度
+
+        //处理表头标题
+        $objPHPExcel->getActiveSheet()->mergeCells('A1:'.$cellKey[count($cellName) -1 ].'1');//合并单元格（如果要拆分单元格是需要先合并再拆分的，否则程序会报错）
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $title);
+        $objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+        $objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setSize(18);
+        $objPHPExcel->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+        $objPHPExcel->getActiveSheet()->getStyle('A1')->getAlignment()->setVertical(\PHPExcel_Style_Alignment::VERTICAL_CENTER);
+    
+        //处理表头
+        foreach ($cellName as $k => $v) {
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellKey[$k].$topNumber, $v);//设置表头数据
+            //$objPHPExcel->getActiveSheet()->freezePane($cellKey[$k].($topNumber+1));//冻结窗口
+            $objPHPExcel->getActiveSheet()->getColumnDimension($cellKey[$k])->setWidth(30);//设置列宽度
+            $objPHPExcel->getActiveSheet()->getStyle($cellKey[$k].$topNumber)->getFont()->setBold(true);//设置是否加粗
+            $objPHPExcel->getActiveSheet()->getStyle($cellKey[$k].$topNumber)->getAlignment()->setVertical(\PHPExcel_Style_Alignment::VERTICAL_CENTER);//垂直居中
+        }
+        //处理数据
+        foreach ($data as $key => $value) {
+            $cell = explode("`", $value);
+            array_shift($cell);
+            foreach ($cell as $k => $v) {
+                $objPHPExcel->getActiveSheet()->setCellValue($cellKey[$k].($key+1+$topNumber), $v);
+            }
+        }
+
+        //处理统计数据
+        foreach ($total as $key => $value) {
+            if ($key ==0) {
+                $cell = explode(" ", $value);
+                foreach ($cell as $k => $v) {
+                    $objPHPExcel->getActiveSheet()->setCellValue($cellKey[$k].($data_nums+2+$topNumber), $v);
+                    $objPHPExcel->getActiveSheet()->getStyle($data_nums+2+$topNumber)->getFont()->setBold(true);//设置是否加粗
+                    $objPHPExcel->getActiveSheet()->getStyle($data_nums+2+$topNumber)->getAlignment()->setVertical(\PHPExcel_Style_Alignment::VERTICAL_CENTER);//垂直居中
+                }
+            } else {
+                $cell = explode("`", $value);
+                //dump($cell);
+                array_shift($cell);
+                //dump($cell);
+                foreach ($cell as $k => $v) {
+                    $objPHPExcel->getActiveSheet()->setCellValue($cellKey[$k].($data_nums+3+$topNumber), $v);
+                }
+            }
+        }
+        //exit();
+
+
+        //导出execl
+        header('pragma:public');
+        header('Content-type:application/vnd.ms-excel;charset=utf-8;name="'.$xlsTitle.'.xls"');
+        header("Content-Disposition:attachment;filename=$fileName.xls");//attachment新窗口打印inline本窗口打印
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $objWriter->save('php://output');
+        exit;
+
+        //这是另一种玩法 可以根据单元个数 指定输出的信息 因为 当日所有订单 和 当日退款的订单 头部信息不同 所以这个固定的 24 个单元 要小心 要根据选项而修改才行
+        // $reponse = explode("\r\n", $reponse);
+        // dump($reponse);
+        // $result = array();
+        // $reponse = str_replace(",", " ", $reponse);
+        // $reponse = explode("`", $reponse);
+        // $total_order_count =(count($reponse) - 6) / 24;
+        // for ($i = 0; $i< $total_order_count; $i++) {
+        //     $base_index = 24 * $i;
+        //     $result[$reponse[$base_index + 7]] = array(
+        //         'wechat_order_no' => $reponse[$base_index + 6],
+        //         'order_count' => $reponse[$base_index + 13],
+        //         'order_discount' => $reponse[$base_index + 23]
+        //     );
+        // }
+        //return $result;
     }
 }
