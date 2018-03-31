43c17c711a763632b895f90203a4e2dc25a31f07
elickzhao <xwiwi@foxmail.com>
2018-03-31 11:59:36 +0800

    以前在线上改的 忘记同步本地的 小程序分类排序

--- App/Api/Controller/CategoryController.class.php
+++ App/Api/Controller/CategoryController.class.php
@@ -1,28 +1,33 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
 namespace Api\Controller;
+
 use Think\Controller;
-class CategoryController extends PublicController {
-	//***************************
-	// 产品分类
-	//***************************
-    public function index(){
-    	$list = M('category')->where('tid=1')->field('id,tid,name')->select();
+
+class CategoryController extends PublicController
+{
+    //***************************
+    // 产品分类
+    //***************************
+    public function index()
+    {
+        $list = M('category')->where('tid=1')->field('id,tid,name')->order('sort desc,id asc')->select();
         $catList = M('category')->where('tid='.intval($list[0]['id']))->field('id,name,bz_1')->select();
         foreach ($catList as $k => $v) {
             $catList[$k]['bz_1'] = __DATAURL__.$v['bz_1'];
         }
 
-    	//json加密输出
-		//dump($json);
-		echo json_encode(array('status'=>1,'list'=>$list,'catList'=>$catList));
+        //json加密输出
+        //dump($json);
+        echo json_encode(array('status'=>1,'list'=>$list,'catList'=>$catList));
         exit();
     }
 
     //***************************
     // 产品分类
     //***************************
-    public function getcat(){
+    public function getcat()
+    {
         $catid = intval($_REQUEST['cat_id']);
         if (!$catid) {
             echo json_encode(array('status'=>0,'err'=>'没有找到产品数据.'));
@@ -39,5 +44,4 @@ class CategoryController extends PublicController {
         echo json_encode(array('status'=>1,'catList'=>$catList));
         exit();
     }
-
 }
