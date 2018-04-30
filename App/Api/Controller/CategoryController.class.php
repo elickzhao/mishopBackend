2301dc0ad0343ed0bd5ba8ca9e365bd48364adf2
elickzhao <xwiwi@foxmail.com>
2018-04-30 22:27:00 +0800

    修复分类页隐藏bug

--- App/Api/Controller/CategoryController.class.php
+++ App/Api/Controller/CategoryController.class.php
@@ -12,14 +12,16 @@ class CategoryController extends PublicController
     public function index()
     {
         $list = M('category')->where('tid=1 AND bz_4=0 ')->field('id,tid,name')->order('sort desc,id asc')->select();
-        $catList = M('category')->where('tid='.intval($list[0]['id'].' AND bz_4=0 '))->field('id,name,bz_1')->order('sort desc,id asc')->select();
+        $catList = M('category')->where('tid='.intval($list[0]['id']).' AND bz_4=0 ')->field('id,name,bz_1')->order('sort desc,id asc')->select();
         foreach ($catList as $k => $v) {
             $catList[$k]['bz_1'] = __DATAURL__.$v['bz_1'];
         }
+        //$sql = M('category')->getlastsql();
 
         //json加密输出
         //dump($json);
-        echo json_encode(array('status'=>1,'list'=>$list,'catList'=>$catList));
+        //echo json_encode(array('status'=>1,'list'=>$list,'catList'=>$catList,'currType'=>intval($list[0]['id']),'sql'=>$sql));
+        echo json_encode(array('status'=>1,'list'=>$list,'catList'=>$catList,'currType'=>intval($list[0]['id'])));
         exit();
     }
 
